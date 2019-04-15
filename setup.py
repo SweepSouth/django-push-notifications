3e023a4f366e515fe9337cef27173da8befeae77
Adrian Moisey <adrian@changeover.za.net>
2019-04-15 11:00:21 +0200

    Use find_packages

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='django-push-notifications',
     version='1.4.2',
     packages=['push_notifications'],
+    packages=find_packages(),
 )
