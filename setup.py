1f1408b15db5729c6022940f3d42aa8bda8d50d7
Adrian Moisey <adrian@changeover.za.net>
2019-04-15 11:02:18 +0200

    I am a noob, that's why

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ from setuptools import setup, find_packages
 setup(
     name='django-push-notifications',
     version='1.4.2',
-    packages=['push_notifications'],
     packages=find_packages(),
 )
